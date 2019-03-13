d284b1d670e4d47741ef7f0cdfdc0990a501f88d
PRuedaApplicaster <p.rueda@applicaster.com>
2019-03-13 17:18:01 +0000

    Fix not handling notifications (#1)
    The behaviour when we were receiving a push notification was not being handled. This logic is in the SDK so the plugin should not override this logic assigning itself as the delegate of the UNUserNotificationCenter.
    We added also the possibility to print the firebase token so we can send custom push notifications to individual users.

--- iOS/ZappPushPluginFirebase/APPushProviderFirebase.swift
+++ iOS/ZappPushPluginFirebase/APPushProviderFirebase.swift
@@ -21,8 +21,10 @@ open class APPushProviderFirebase: ZPPushProvider {
         if (FirebaseApp.app() == nil) {
             FirebaseApp.configure()
         }
+        Messaging.messaging().delegate = self
+        
+        //Don't assign the UNUserNotificationCenter delegate because we are already handling the logic in the SDK
         if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
             let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
             UNUserNotificationCenter.current().requestAuthorization(
                 options: authOptions,
@@ -37,16 +39,9 @@ open class APPushProviderFirebase: ZPPushProvider {
     }
 }
 
-@available(iOS 10, *)
-extension APPushProviderFirebase : UNUserNotificationCenterDelegate {
+extension APPushProviderFirebase : MessagingDelegate {
     
-    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .badge, .sound])
-    }
-    
-    public func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
+    open func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        NSLog("Firebase registration token %@", fcmToken); //NSLog instead of print so it can be visualized without XCode
     }
 }
