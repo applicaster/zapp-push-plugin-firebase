5b962b48983eafb662fd89704545b90f5063953f
Pablo Rueda <p.rueda@applicaster.com>
2019-05-20 16:02:13 +0100

    Added functionality to subscribe and unsubscribe to topics.

--- Android/firebasepushpluginandroid/src/main/java/com/applicaster/firebasepushpluginandroid/FirebasePushProvider.kt
+++ Android/firebasepushpluginandroid/src/main/java/com/applicaster/firebasepushpluginandroid/FirebasePushProvider.kt
@@ -3,12 +3,15 @@ package com.applicaster.firebasepushpluginandroid
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.content.IntentFilter
 import android.os.Build
 import com.applicaster.plugin_manager.push_plugin.PushContract
 import com.applicaster.plugin_manager.push_plugin.helper.PushPluginsType
 import com.applicaster.plugin_manager.push_plugin.listeners.PushTagLoadedI
 import com.applicaster.plugin_manager.push_plugin.listeners.PushTagRegistrationI
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlin.coroutines.experimental.suspendCoroutine
 
 class FirebasePushProvider : PushContract {
 
@@ -45,39 +48,67 @@ class FirebasePushProvider : PushContract {
         tag: MutableList<String>?,
         pushTagRegistrationListener: PushTagRegistrationI?
     ) {
-        //We should iterate through all tags calling this method. And once all methods returned, call the pushTagRegistrationListener.
-        //We should use couritines fore that
+           launch{ registerAll(tag, pushTagRegistrationListener)}
+    }
 
-        FirebaseMessaging.getInstance().subscribeToTopic(topic).addOnCompleteListener { task ->
-                    //cont.resume(task.isSuccessful)
-                    Log.d(TAG, "isSuccessful " + task.isSuccessful)
-                }
+
+    override fun removeTagToProvider(
+            context: Context?,
+            tag: MutableList<String>?,
+            pushTagRegistrationListener: PushTagRegistrationI?
+    ) {
+        launch{ unregisterAll(tag, pushTagRegistrationListener)}
     }
 
     override fun setPluginParams(params: MutableMap<Any?, Any?>?) {
         pluginsParamsMap = params
     }
 
-    override fun removeTagToProvider(
-        context: Context?,
-        tag: MutableList<String>?,
-        pushTagRegistrationListener: PushTagRegistrationI?
-    ) {
-        //We should iterate through all tags calling this method. And once all methods returned, call the pushTagRegistrationListener.
-        //We should use couritines fore that
-        FirebaseMessaging.getInstance().unsubscribeFromTopic(topic).addOnCompleteListener { task ->
-                    //cont.resume(task.isSuccessful)
-                    Log.d(TAG, "isSuccessful " + task.isSuccessful)
-                }
-    }
-
     override fun getTagList(context: Context?, listener: PushTagLoadedI?) {
 
     }
+
+    //region Private methods
 
     private fun getPluginParamByKey(key: String): String {
         if (pluginsParamsMap?.containsKey(key) == true)
             return pluginsParamsMap?.get(key).toString()
         return ""
     }
+
+    suspend private fun registerAll(tag: MutableList<String>?,
+                                    pushTagRegistrationListener: PushTagRegistrationI?){
+        var totalResult = true;
+        tag?.forEach {
+            var result = async{register(it) }.await()
+            totalResult = totalResult && result
+        }
+        pushTagRegistrationListener?.pushRregistrationTagComplete(PushPluginsType.applicaster, totalResult)
+    }
+
+    suspend private fun register(topic : String): Boolean =
+            suspendCoroutine { cont ->
+                FirebaseMessaging.getInstance().subscribeToTopic(topic).addOnCompleteListener { task ->
+                    cont.resume(task.isSuccessful)
+                }
+            }
+
+    suspend private fun unregisterAll(tag: MutableList<String>?,
+                                    pushTagRegistrationListener: PushTagRegistrationI?){
+        var totalResult = true;
+        tag?.forEach {
+            var result = async{unregister(it) }.await()
+            totalResult = totalResult && result
+        }
+        pushTagRegistrationListener?.pushRregistrationTagComplete(PushPluginsType.applicaster, totalResult)
+    }
+
+    suspend private fun unregister(topic : String): Boolean =
+            suspendCoroutine { cont ->
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(topic).addOnCompleteListener { task ->
+                    cont.resume(task.isSuccessful)
+                }
+            }
+
+    //endregion
 }
